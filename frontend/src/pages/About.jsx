6fe47822116fdaff3cecc78f0c56632adba13341
Mohammad Danish <danish.sab05@gmail.com>
2026-05-27 03:02:20 +0530

    feat: add about and projects pages

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div className="min-h-screen pt-24 px-6">
+      <h1 className="text-5xl text-orange-500 font-bold">About</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ frontend/src/pages/Projects.jsx
@@ -0,0 +1,9 @@
+function Projects() {
+  return (
+    <div className="min-h-screen pt-24 px-6">
+      <h1 className="text-5xl text-orange-500 font-bold">Projects</h1>
+    </div>
+  );
+}
+
+export default Projects;
