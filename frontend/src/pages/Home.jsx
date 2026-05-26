24067d633cc51515a08403fbd8080e55c58361c8
Mohammad Danish <danish.sab05@gmail.com>
2026-05-27 02:58:39 +0530

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main className="min-h-screen pt-24 px-6">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-6xl font-bold text-orange-500">Home Page</h1>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
