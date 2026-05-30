39cd23596a80e49d9440e0afd63f81fb07454ac6
Mohammad Danish <danish.sab05@gmail.com>
2026-05-31 00:41:18 +0530

    feat:add all the skills with  cotegory

--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -1,8 +1,77 @@
-export default function About() {
-  return (
-    <div className="min-h-screen pt-32 px-6">
-      <h1 className="text-5xl font-black">Skills Page</h1>
+import { motion } from "framer-motion";
 
-    </div>
-  );
-}
+import {
+  FaReact,
+  FaNodeJs,
+  FaPython,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaGithub,
+  FaGitAlt,
+  FaFigma,
+} from "react-icons/fa";
+
+import {
+  SiFastapi,
+  SiTailwindcss,
+  SiMongodb,
+  SiMysql,
+  SiExpress,
+  SiCanva,
+  SiPostman,
+  SiVisualstudiocode,
+} from "react-icons/si";
+
+
+const skillCategories = [
+  {
+    title: "Frontend Development",
+    skills: [
+      { name: "HTML5", icon: <FaHtml5 />, level: 98 },
+      { name: "CSS3", icon: <FaCss3Alt />, level: 95 },
+      { name: "JavaScript", icon: <FaJs />, level: 92 },
+      { name: "React.js", icon: <FaReact />, level: 95 },
+      { name: "Tailwind CSS", icon: <SiTailwindcss />, level: 96 },
+    ],
+  },
+
+  {
+    title: "Backend Development",
+    skills: [
+      { name: "Node.js", icon: <FaNodeJs />, level: 90 },
+      { name: "Express.js", icon: <SiExpress />, level: 88 },
+      { name: "FastAPI", icon: <SiFastapi />, level: 90 },
+    ],
+  },
+
+  {
+    title: "Programming Languages",
+    skills: [
+      { name: "C", icon: "C", level: 85 },
+      { name: "C++", icon: "C++", level: 88 },
+      { name: "Python", icon: <FaPython />, level: 92 },
+      { name: "JavaScript", icon: <FaJs />, level: 92 },
+    ],
+  },
+
+  {
+    title: "Databases",
+    skills: [
+      { name: "MongoDB", icon: <SiMongodb />, level: 84 },
+      { name: "MySQL", icon: <SiMysql />, level: 86 },
+    ],
+  },
+
+  {
+    title: "Tools & Design",
+    skills: [
+      { name: "GitHub", icon: <FaGithub />, level: 92 },
+      { name: "Git", icon: <FaGitAlt />, level: 88 },
+      { name: "Postman", icon: <SiPostman />, level: 90 },
+      { name: "VS Code", icon: <SiVisualstudiocode />, level: 96 },
+      { name: "Canva", icon: <SiCanva />, level: 85 },
+      { name: "Figma", icon: <FaFigma />, level: 80 },
+    ],
+  },
+];
