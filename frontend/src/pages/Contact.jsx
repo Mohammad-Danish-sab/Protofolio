867830ce9aa86ebdd2faa4001506e04e4562e23f
Mohammad Danish <danish.sab05@gmail.com>
2026-06-02 01:42:45 +0530

    feat:add Quick Contact Cards in contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -45,3 +45,41 @@ import {
   </div>
 </div>;
 
+<div className="grid grid-cols-2 gap-4 mb-10">
+  <a
+    href="mailto:danish.sab05@gmail.com"
+    className="
+    bg-linear-to-r
+    from-cyan-500/10
+    to-teal-500/10
+    border border-cyan-400/20
+    rounded-3xl
+    p-5
+    "
+  >
+    <h3 className="font-bold">Email Me</h3>
+
+    <p className="text-zinc-400 text-sm mt-2">Fast Response</p>
+  </a>
+
+  <a
+    href="https://wa.me/918294534533"
+    target="_blank"
+    rel="noreferrer"
+    className="
+    bg-linear-to-r
+    from-green-500/10
+    to-emerald-500/10
+    border border-green-400/20
+    rounded-3xl
+    p-5
+    "
+  >
+    <h3 className="font-bold">WhatsApp</h3>
+
+    <p className="text-zinc-400 text-sm mt-2">Instant Chat</p>
+  </a>
+</div>;
+
+
+
