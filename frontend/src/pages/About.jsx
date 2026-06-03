52e59a544b7401aebe581206cf5864afd59a8505
Mohammad Danish <danish.sab05@gmail.com>
2026-06-04 00:31:34 +0530

    feat:add data in about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,57 @@
 import { motion } from "framer-motion";
 import profileImg from "../assets/Images/img1.jpeg";
 
-import { FaReact, FaNodeJs, FaPython, FaDownload } from "react-icons/fa";
+import {
+  FaReact,
+  FaNodeJs,
+  FaPython,
+  FaDownload,
+} from "react-icons/fa";
 
-import { SiFastapi, SiMongodb, SiTailwindcss } from "react-icons/si";
+import {
+  SiFastapi,
+  SiMongodb,
+  SiTailwindcss,
+} from "react-icons/si";
+
+
+const achievements = [
+  { number: "20+", title: "Projects" },
+  { number: "10+", title: "Technologies" },
+  { number: "2+", title: "Years Learning" },
+  { number: "100%", title: "Dedication" },
+];
+
+const services = [
+  "Modern UI/UX",
+  "Full Stack Development",
+  "REST API Development",
+  "AI Powered Applications",
+];
+
+const techStack = [
+  {
+    icon: <FaReact />,
+    name: "React",
+  },
+  {
+    icon: <FaNodeJs />,
+    name: "Node.js",
+  },
+  {
+    icon: <FaPython />,
+    name: "Python",
+  },
+  {
+    icon: <SiFastapi />,
+    name: "FastAPI",
+  },
+  {
+    icon: <SiTailwindcss />,
+    name: "Tailwind",
+  },
+  {
+    icon: <SiMongodb />,
+    name: "MongoDB",
+  },
+];
