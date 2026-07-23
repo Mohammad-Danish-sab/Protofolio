b149f1f5b574b9c14c9f3eb750b72753b8a44ead
Mohammad Danish <danish.sab05@gmail.com>
2026-07-23 22:22:16 +0530

    feat:add upload and cloudinary

---
+++ backend/app/core/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
