0de0511e6fb8e16511c6d0426aaad2fdffa3042a
Mohammad Danish <danish.sab05@gmail.com>
2026-08-07 04:00:22 +0530

    feat:add the  dashboardlayout page

--- frontend/src/admin/components/Layout/DashboardLayout.jsx
+++ frontend/src/admin/components/Layout/DashboardLayout.jsx
@@ -3,13 +3,17 @@ import Topbar from "../dashboard/Topbar";
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="min-h-screen bg-slate-950 flex">
+    <div className="min-h-screen bg-slate-950">
+      {/* Sidebar */}
       <Sidebar />
 
-      <div className="flex-1 flex flex-col ml-72">
+      {/* Main Content */}
+      <div className="ml-72 min-h-screen flex flex-col">
+        {/* Top Navigation */}
         <Topbar />
 
-        <main className="p-8">{children}</main>
+        {/* Page Content */}
+        <main className="flex-1 p-8">{children}</main>
       </div>
     </div>
   );
